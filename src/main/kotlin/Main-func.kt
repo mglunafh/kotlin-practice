db47691ce88d4c1554bae9b5d5ae75ab394aacbf
ufispb <ufispb@gmail.com>
2023-04-16 19:10:28 +0300

    function as a parameter

--- src/main/kotlin/Main-func.kt
+++ src/main/kotlin/Main-func.kt
@@ -1,12 +1,20 @@
 fun main(args: Array<String>) {
-    println("A hero enters the tavern. What is their name?")
+    narrateFancy1("A hero enters the tavern. What is their name?",
+        { message -> "\u001b[33;1m$message\u001b[0m" })
     val heroName = readlnOrNull() ?: "kek"
-    println(loudNarration("Hello, $heroName!", "sneaky"))
 
-    val specialChar = 'o'
-    val commonChar = 'q'
-    val someNumber = heroName.count { c -> c == specialChar }
-    val anotherNumber = heroName.count { it == commonChar }
-    narrate("Number of special chars '$specialChar': $someNumber")
-    narrate("Number of usual chars '$commonChar': $anotherNumber")
+    // The last parameter of `narrateFancy2` is function,
+    // so it's allowed to move it out the parenthesis.
+    narrateFancy2("Hello, $heroName!") {message -> "\u001b[31m$message\u001b[0m"}
+
+    val someChars = arrayOf('o', 'q', 'f', 'g')
+    for (char in someChars) {
+        changeNarratorMood()
+        numberOfChars(heroName, char)
+    }
+}
+
+fun numberOfChars(name: String, c: Char) {
+    val someNumber = name.lowercase().count { it == c }
+    narrate("Number of chars '$c': $someNumber")
 }
--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,11 +1,18 @@
+import java.util.Random
 
-// lambda return value is the type of the last expression, in this case -- String
-// return is prohibited in lambdas
+// lambda return value is the type of the last expression, in this case -- String;
+// return is prohibited in lambdas.
 val narrationLambda: (String) -> String = { message ->
     val numberOfExclamations = 3
     message.uppercase() + "!".repeat(numberOfExclamations)
 }
 
+// declare the type of arguments in the body
+val anotherNarration = { message: String ->
+    message + ".".repeat(3)
+}
+
+// declare the lambda with several arguments
 val loudNarration: (String, String) -> String = { message, tone ->
     when (tone) {
         "excited" ->  {
@@ -18,8 +25,47 @@ val loudNarration: (String, String) -> String = { message, tone ->
     }
 }
 
+// Currying ?..
 val sneakyNarration: (String) -> String = { loudNarration(it, "sneaky") }
 
+var narrationModifier: (String) -> String = { it }
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+    val random = Random()
+    when (random.nextInt(5)) {
+        1 -> {
+            mood = "loud"
+            modifier = narrationLambda
+        }
+        2 -> {
+            mood = "tired"
+            modifier = { it.lowercase().replace(" ", "... ") }
+        }
+        3 -> {
+            mood = "unsure"
+            modifier = { message -> "$message?"}
+        }
+        else -> {
+            mood = "professional"
+            modifier = { message -> "$message."}
+        }
+    }
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
+}
+
 fun narrate(message: String) {
-    println(narrationLambda(message))
+    println(narrationModifier(message))
+}
+
+// function which takes function as a parameter with a default value
+fun narrateFancy1(message: String, modifier: (String) -> String = { narrationModifier(it)}) {
+    println(modifier(message))
+}
+
+// function which takes function as a parameter with a default value, more concise syntax
+fun narrateFancy2(message: String, modifier: (String) -> String = narrationModifier) {
+    println(modifier(message))
 }
